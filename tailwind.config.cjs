9889fe6f84edc62e3accfabe389b2024cce0222e
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2023-03-27 02:34:45 +0200

    corrections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx,js,jsx,css,scss,sas}"],
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
