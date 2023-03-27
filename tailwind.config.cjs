4cc605dac1338cb12d1ec04d42dcdfc9d880bbe3
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2023-03-27 02:37:44 +0200

    commit du dist

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx,js,jsx,css,scss,sas}"],
-  purge: [
-    './src/components/**/*.{js,ts,jsx,tsx}'
-  ],
   theme: {
     extend: {},
   },
